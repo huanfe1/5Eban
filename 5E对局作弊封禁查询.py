c10cd5d880ae6aab00900a3df85701a76821c51d
幻非 <41602338+huanfeick@users.noreply.github.com>
2020-03-10 16:33:37 +0800

    1.1
    5E对局作弊封禁查询1.1

--- 5E对局作弊封禁查询.py
+++ 5E对局作弊封禁查询.py
@@ -2,43 +2,47 @@ import urllib.request
 import re
 import webbrowser
 
-
+i = 0
 
 #ban-type3-simplified-chinese 表示非法行为冻结
 #ban-type4-simplified-chinese 表示作弊封禁
-def get(url0):
-	response0 = urllib.request.urlopen(url0)
-	#print("代码：",response0.getcode()) 	#代码如果是200表示获取成功
-	data0 = response0.read()	#获取网页代码
-	if data0 != None:
-		data0 = data0.decode('utf-8')	#转换编码
-		reg0 = r'<a href="(.+?)" target="_blank">查看详情</a>'
-		imreg0 = re.compile(reg0)
-		imglist = re.findall(imreg0,data0)
-		del imglist[10:12] #删除列表中11号到12行的内容
-		#getwz = '\n'.join(imglist) #'\n' 输入换行   #可以直接用getwz[第几行]来代替
-		return(imglist)
+
+def get(userurl):
+	response = urllib.request.urlopen(userurl) #获取网页代码
+	data = response.read() ###次行内容不太清楚，后续查查
+	if data != None:
+		data = data.decode('utf-8')#转换编码
+		reg = r'<a href="(.+?)" target="_blank">查看详情</a>'
+		rereg = re.compile(reg)
+		relist = re.findall(rereg,data)
+		del relist[10:12] #还没有做如果战绩少于10场怎么办
+		print(relist)
+		return(relist)
 
 
 
-def search(url):
-	response = urllib.request.urlopen(url)
-	#print("代码：",response.getcode())	#代码如果是200表示获取成功
+def search(dataurl):#查找该战绩是否含有封禁玩家
+	response = urllib.request.urlopen(dataurl)
 	data = response.read()
 	data = data.decode('utf-8')
-	regban4 = 'ban-type4'
-	regban3 = 'ban-type3'
-	findresult = data.find(regban4)
-	if findresult != -1:
-		return(url)
+	bantype3 = 'ban-type3'
+	bantype4 = 'ban-type4'
+	findban = data.find(bantype4)#括号内容的数字为查找的封禁类型,后续加可选择
+	if findban != -1:
+		return(dataurl)
 
-url0 = "https://www.5ewin.com/data/player/4132633imxyu4"
-getwz = get(url0)
+url = "https://www.5ewin.com/data/player/4132633imxyu4"#玩家战绩网址
 
-i = 0
-for i in range(10):
-	print(getwz[i])#打印10场战绩
-	getbanwz =  search(getwz[i])
+getwz = get(url)
+
+for i in range(10):#不点刷新之前只显示最后10的战绩
+	getbanwz = search(getwz[i])
 	if getbanwz != None:
 		print(getbanwz)
 	i+1
+
+
+
+#加上少于10场怎么办
+#可选择玩家网址
+#模拟点击刷新网址获取后续的战绩
