87bd634468984b70fb24d4ea54ba49f22ba2a8a5
幻非 <41602338+huanfeick@users.noreply.github.com>
2020-03-10 16:34:08 +0800

    1.2
    5E对局作弊封禁查询1.2

--- 5E对局作弊封禁查询.py
+++ 5E对局作弊封禁查询.py
@@ -1,48 +1,69 @@
-import urllib.request
+import urllib
+import requests
 import re
 import webbrowser
+import json
+import os
 
+
+
+def pages(uid,p):#变化网址页数
+	url = "https://www.5ewin.com/api/data/match_list/" + uid + "?yyyy=2020&page=" + p
+	return(url)
+
+
+def get(url,i): #i表示第10场内的第几场
+	response = urllib.request.urlopen(url)
+	getdata = response.read().decode('utf-8')
+	timename = json.loads(getdata)["data"][i]["match_code"]
+	return(timename)
+
+
+def findnumber(url): #检测该10场数据是否少于10场
+	response = urllib.request.urlopen(url)
+	getdata = response.read().decode('utf-8')
+	sub = "match_code"
+	num = getdata.count(sub)#number正常为10
+	return(num)
+
+
+def search(url):
+	response = urllib.request.urlopen(url)
+	#print("代码：",response.getcode())#代码如果是200表示获取成功
+	getdata = response.read().decode('utf-8')
+	#ban1 = "ban-type1" #违规行为封禁
+	#ban3 = "ban-type3" #非法行为冻结
+	#ban4 = "ban-type4" #作弊封禁
+	ban = "ban-type4"
+	searchresult = getdata.find(ban)
+	#print(searchresult)
+	if searchresult != -1:
+		return(url)
+
+
+
+userall = input("请输入5E玩家地址:")
+
+
+reg = r'https://www.5ewin.com/data/player/(.*)'
+imgre = re.compile(reg)
+userid = ''.join(re.findall(imgre,userall))
+
+page = 1
 i = 0
-
-#ban-type3-simplified-chinese 表示非法行为冻结
-#ban-type4-simplified-chinese 表示作弊封禁
-
-def get(userurl):
-	response = urllib.request.urlopen(userurl) #获取网页代码
-	data = response.read() ###次行内容不太清楚，后续查查
-	if data != None:
-		data = data.decode('utf-8')#转换编码
-		reg = r'<a href="(.+?)" target="_blank">查看详情</a>'
-		rereg = re.compile(reg)
-		relist = re.findall(rereg,data)
-		del relist[10:12] #还没有做如果战绩少于10场怎么办
-		print(relist)
-		return(relist)
-
-
-
-def search(dataurl):#查找该战绩是否含有封禁玩家
-	response = urllib.request.urlopen(dataurl)
-	data = response.read()
-	data = data.decode('utf-8')
-	bantype3 = 'ban-type3'
-	bantype4 = 'ban-type4'
-	findban = data.find(bantype4)#括号内容的数字为查找的封禁类型,后续加可选择
-	if findban != -1:
-		return(dataurl)
-
-url = "https://www.5ewin.com/data/player/4132633imxyu4"#玩家战绩网址
-
-getwz = get(url)
-
-for i in range(10):#不点刷新之前只显示最后10的战绩
-	getbanwz = search(getwz[i])
-	if getbanwz != None:
-		print(getbanwz)
-	i+1
-
-
-
-#加上少于10场怎么办
-#可选择玩家网址
-#模拟点击刷新网址获取后续的战绩
+for x in range(10):
+	allpage = pages(userid,str(page))#获取到的总体战绩页
+	page = page + 1
+	number = findnumber(allpage)
+	if number == 0: #检测剩余战绩是否为零
+		break
+	#print(allpage)
+	for i in range(number):
+		data = get(allpage,i)
+		data = "https://www.5ewin.com/data/match/" + data
+		#print(data)
+		i + 1
+		banwz = search(data)
+		if banwz != None:
+			print(banwz)
+input("按任意键退出")
